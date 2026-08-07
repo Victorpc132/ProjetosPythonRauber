6c60c31e7bb29aa72e2c7484f883c16f3474a380
Victorpc132 <vmano9251@gmail.com>
2026-08-07 16:59:29 -0300

    Add files via upload

--- ex3.py
+++ ex3.py
@@ -1,8 +1,15 @@
-nome =input("Digite o nome: ")
-named = nome.split(" ")
-size = len(named)
-nomeFormat = named.split(size-1)
+name = input("Digite Seu Nome: ")
 
-print(size)
-print(nomeFormat)
+def ajeitar_nome(nomepar="André da silva Camargo"):
+    named = nomepar.split(" ")
+    size = len(named)-1
+    ultnome = named[size]
+    ultnome= ultnome.upper()
+    iniciais = ""
+    for i in range(size):
+        if named[i] not in ['da', 'de', 'do', 'das', 'dos']:
+           iniciais += named[i][0].upper()+". "
 
+    return print(ultnome," "+iniciais)
+
+ajeitar_nome(name)
